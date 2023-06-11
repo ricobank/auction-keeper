5cf60fac0013575a489eeb9f4cc57d4b74906459
kbrav <>
2023-06-11 13:55:57 -0400

    grant, fill_flop passes

--- test/Strat.t.sol
+++ test/Strat.t.sol
@@ -5,6 +5,7 @@ import "forge-std/Test.sol";
 
 import { RicoSetUp, Guy, WethLike } from '../lib/ricobank/test/RicoHelper.sol';
 import { Vat } from '../lib/ricobank/src/vat.sol';
+import { Vow} from '../lib/ricobank/src/vow.sol';
 import '../lib/ricobank/test/UniHelper.sol';
 import { IERC721, INonfungiblePositionManager } from '../lib/ricobank/test/Univ3Interface.sol';
 import { Strat } from '../src/strat.sol';
@@ -47,6 +48,7 @@ contract StratTest is Test, RicoSetUp {
         make_bank();
         init_gold();
         init_dai();
+        gold.approve(bank, UINT256_MAX);
         vm.prank(VAULT);
         dai.transfer(self, 100000 * WAD);
 
@@ -78,40 +80,64 @@ contract StratTest is Test, RicoSetUp {
         strat = new Strat(bank);
         strat.setSwapRouter(router);
         bytes memory fore; bytes memory rear;
-        /*
-        address [] memory addr2 = new address[](2);
-        uint24  [] memory fees1 = new uint24 [](1);
-        addr2[0] = arisk;
-        addr2[1] = arico;
-        fees1[0] = 3000;
-        (fore, rear) = create_path(addr2, fees1);
-        strat.setPath(arisk, arico, fore, rear);
-        addr2[0] = arico;
-        addr2[1] = arisk;
-        (fore, rear) = create_path(addr2, fees1);
-        strat.setPath(arico, arisk, fore, rear);
-       */
+        {
+            address [] memory addr2 = new address[](2);
+            uint24  [] memory fees1 = new uint24 [](1);
+            addr2[0] = arisk;
+            addr2[1] = arico;
+            fees1[0] = 3000;
+            (fore, rear) = create_path(addr2, fees1);
+            strat.setPath(arisk, arico, fore, rear);
+            addr2[0] = arico;
+            addr2[1] = arisk;
+            (fore, rear) = create_path(addr2, fees1);
+            strat.setPath(arico, arisk, fore, rear);
+        }
 
-        address [] memory addr3 = new address[](3);
-        uint24  [] memory fees2 = new uint24 [](2);
-        addr3[0] = agold;
-        addr3[1] = DAI;
-        addr3[2] = arico;
-        fees2[0] = 3000;
-        fees2[1] = 500;
-        (fore, rear) = create_path(addr3, fees2);
-        strat.setPath(agold, arico, fore, rear);
+        {
+            address [] memory addr3 = new address[](3);
+            uint24  [] memory fees2 = new uint24 [](2);
+            addr3[0] = agold;
+            addr3[1] = DAI;
+            addr3[2] = arico;
+            fees2[0] = 3000;
+            fees2[1] = 500;
+            (fore, rear) = create_path(addr3, fees2);
+            strat.setPath(agold, arico, fore, rear);
+        }
+
+        strat.grant();
     }
 
     function test_fill_flip() public {
-        gold.approve(bank, UINT256_MAX);
         feedpush(grtag, bytes32(RAY * 2), UINT256_MAX);
         Vat(bank).frob(gilk, self, abi.encodePacked(int(25000 * WAD)), int(25000 * WAD));
         feedpush(grtag, bytes32(RAY / 4), UINT256_MAX);
         uint ricobefore = rico.balanceOf(self);
         uint goldbefore = gold.balanceOf(self);
+        assertEq(rico.balanceOf(address(strat)), 0);
+        assertEq(gold.balanceOf(address(strat)), 0);
         strat.fill_flip(gilk, self);
         assertGe(rico.balanceOf(self), ricobefore);
         assertGe(gold.balanceOf(self), goldbefore);
+    }
+
+    function test_fill_flap() public {
+        feedpush(RICO_RISK_TAG, bytes32(RAY), UINT256_MAX);
+        Vat(bank).frob(gilk, self, abi.encodePacked(int(25000 * WAD)), int(25000 * WAD));
+        skip(BANKYEAR);
+        Vat(bank).drip(gilk);
+
+        uint ricobefore = rico.balanceOf(self);
+        uint riskbefore = risk.balanceOf(self);
+        assertEq(rico.balanceOf(address(strat)), 0);
+        assertEq(risk.balanceOf(address(strat)), 0);
+        strat.fill_flap();
+        assertGe(rico.balanceOf(self), ricobefore);
+        assertGe(risk.balanceOf(self), riskbefore);
+    }
+
+    function test_fill_flop() public {
+
     }
 }
