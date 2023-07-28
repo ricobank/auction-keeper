1594728b9e9434588b3e5e6ce502a631aacf6398
kbrav <3114369+kbrav@users.noreply.github.com>
2023-07-28 13:28:59 -0400

    uni swapper (#21)
    Co-authored-by: kbrav <>

--- jstest/strat-test.ts
+++ jstest/strat-test.ts
@@ -1,4 +1,4 @@
-const debug = require('debug')('rico:test')
+const debug = require('debug')('keeper:test')
 import { expect as want } from 'chai'
 
 import * as hh from 'hardhat'
@@ -18,6 +18,8 @@ const dpack = require('@etherpacks/dpack')
 const bn2b32 = (bn) => hexZeroPad(bn.toHexString(), 32)
 const i0 = Buffer.alloc(32) // ilk 0 id
 const TAG = Buffer.from('feed'.repeat(16), 'hex')
+const hzp = ethers.utils.hexZeroPad
+const rpaddr = a => a + '00'.repeat(12)
 let worker
 
 let fb, bank, strat, keeper, ploker, weth, rico, mdn, dapp, divider
@@ -33,6 +35,8 @@ describe('keeper', () => {
     let ali, bob, cat
     let amt = wad(10000)
     let DELAY = 1000
+    let ricodaitokid
+    let dai
     before(async () => {
         [ali, bob, cat] = await ethers.getSigners();
         [ALI, BOB, CAT] = [ali, bob, cat].map(signer => signer.address)
@@ -57,14 +61,24 @@ describe('keeper', () => {
         divider = dapp.divider
         nfpm = dapp.nonfungiblePositionManager
         router = dapp.swapRouter
+        dai = dapp.dai
+        debug(`BANK at ${bank.address}`)
 
         debug('set weth feed', ALI, b32('weth:rico').toString(), mdn.address)
+        debug('gem addresses:')
+        debug(`    weth:   ${weth.address}`)
+        debug(`    dai:    ${dai.address}`)
+        debug(`    rico:   ${rico.address}`)
+        debug(`    risk:   ${risk.address}`)
+        debug(`    wsteth: ${dapp.wsteth.address}`)
         await send(bank.filhi, b32('weth'), b32('fsrc'), b32('weth'), ALI + '00'.repeat(12))
         await send(bank.file, b32('flapsrc'), ALI + '00'.repeat(12))
         await send(bank.file, b32('flopsrc'), ALI + '00'.repeat(12))
         debug('set strat router+path')
-        let {fore, rear} = create_path([weth.address, dapp.dai.address, rico.address], [500, 500])
+        let {fore, rear} = create_path([weth.address, dai.address, rico.address], [500, 500])
         await send(strat.setPath, weth.address, rico.address, fore, rear);
+        ({fore, rear} = create_path([dai.address, rico.address], [500]))
+        await send(strat.setPath, dai.address, rico.address, fore, rear);
         ({fore, rear} = create_path([rico.address, risk.address], [3000]))
         await send(strat.setPath, rico.address, risk.address, fore, rear);
         ({fore, rear} = create_path([risk.address, rico.address], [3000]))
@@ -80,9 +94,9 @@ describe('keeper', () => {
           fliptime: DELAY,
           floptime: DELAY * 2,
           flaptime: DELAY * 2,
-          ilks: 'weth',
+          ilks: 'weth;:uninft',
           tol: ray(0.1),
-          minrush: ray(1.2),
+          minprofit: wad(5),
           expected_rico: wad(10),
           expected_risk: wad(10),
           poketime: '100'
@@ -114,14 +128,15 @@ describe('keeper', () => {
         await send(weth.connect(bob).approve, bank.address, ethers.constants.MaxUint256, {gasLimit: 10000000})
         await send(bank.connect(bob).frob, b32('weth'), BOB, dink, amt.mul(2), {gasLimit: 100000000})
         await send(rico.connect(bob).transfer, ALI, await rico.balanceOf(BOB), {gasLimit: 100000000})
-        console.log("NFPM ", nfpm.address)
-        await join_pool({
+
+        let joinres = await join_pool({
             nfpm: nfpm, ethers: ethers, ali: ali,
             a1: { token: rico.address, amountIn: amt },
-            a2: { token: dapp.dai.address, amountIn: amt },
+            a2: { token: dai.address, amountIn: amt },
             fee: 500,
             tickSpacing: 10
         })
+        ricodaitokid = joinres.tokenId
         dink = ethers.utils.solidityPack(["int256"], [amt])
         await send(risk.mint, ALI, amt)
         await join_pool({
@@ -152,7 +167,7 @@ describe('keeper', () => {
 
     const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms))
 
-    it('fill_flip', async () => {
+    it('fill_flip gem', async () => {
 
         await send(weth.deposit, {value: amt})
 
@@ -212,6 +227,37 @@ describe('keeper', () => {
         await send(bank.drip, b32('weth'))
         await delay(DELAY * 2)
         want((await rico.balanceOf(ALI)).gt(ricobefore)).true
+    })
+
+    it('fill_flip uni', async () => {
+        await delay(2000)
+        await send(bank.filk, b32(':uninft'), b32('line'), bn2b32(amt.mul(ray(10))))
+        await send(bank.filhi2,
+            b32(':uninft'), b32('fsrc'), b32(':uninft'), rpaddr(dai.address), rpaddr(ALI)
+        )
+        await send(bank.filhi2,
+            b32(':uninft'), b32('ftag'), b32(':uninft'), rpaddr(dai.address), b32('dai:rico')
+        )
+        await send(bank.filhi2,
+            b32(':uninft'), b32('fsrc'), b32(':uninft'), rpaddr(rico.address), rpaddr(ALI)
+        )
+        await send(bank.filhi2,
+            b32(':uninft'), b32('ftag'), b32(':uninft'), rpaddr(rico.address), b32('ONE')
+        )
+ 
+        await send(fb.push, b32('dai:rico'), bn2b32(ray(1)), ethers.constants.MaxUint256)
+        await send(fb.push, b32('ONE'), bn2b32(ray(1)), ethers.constants.MaxUint256)
+        await delay(DELAY * 2)
+ 
+        await send(nfpm.approve, bank.address, ricodaitokid)
+        let dink = ethers.utils.solidityPack(['uint', 'uint'], [1, ricodaitokid])
+        await send(bank.frob, b32(':uninft'), ALI, dink, amt.mul(3).div(2))
+
+        await delay(DELAY)
+        want(await nfpm.ownerOf(ricodaitokid)).eql(bank.address)
+        await send(fb.push, b32('dai:rico'), bn2b32(ray(0)), ethers.constants.MaxUint256)
+        await delay(DELAY * 2)
+        want(await nfpm.ownerOf(ricodaitokid)).eql(ALI)
     })
 
     /*
